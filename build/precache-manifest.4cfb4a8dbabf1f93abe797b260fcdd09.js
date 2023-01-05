6b3db22eb12dd24a6e47d844b5cb4082e840014a
Anurag <anurag171199@gmail.com>
2023-01-05 18:06:59 +0530

    added build

---
+++ build/precache-manifest.4cfb4a8dbabf1f93abe797b260fcdd09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ee71b28c8c6c8f09167188a074ca375",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0c4f13e7c151574f6e2",
+    "url": "/static/css/main.2b02fe21.chunk.css"
+  },
+  {
+    "revision": "e88ae9b6a65fe1c8aa11",
+    "url": "/static/js/2.fc7f14af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fc7f14af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0c4f13e7c151574f6e2",
+    "url": "/static/js/main.bc98fc30.chunk.js"
+  },
+  {
+    "revision": "72c07dcb21740fe3cff4",
+    "url": "/static/js/runtime-main.b593c7cf.js"
+  }
+]);
